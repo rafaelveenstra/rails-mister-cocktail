4c5e33840c3efa2f367f732e19259c721d47eb30
rafael veenstra <rafaelveenstra@gmail.com>
2019-02-15 09:08:45 -0200

    Rails Mister Cocktail Backend

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -5,19 +5,22 @@ class DosesController < ApplicationController
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
     if @dose.save
+      redirect_to @dose.cocktail
     else
       flash[:dose_errors] = @dose.errors.full_messages
+      redirect_to new_cocktail_dose_path(@cocktail)
     end
-    redirect_to @dose.cocktail
   end
 
   def destroy
     @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
     @dose.destroy
-    redirect_to @dose.cocktail
+    redirect_to @cocktail
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root to: 'cocktails#index'
 
   resources :cocktails do
-    resources :doses, only: %i[index new create]
+    resources :doses, only: %i[new create]
   end
   resources :doses, only: %i[destroy]
 end
